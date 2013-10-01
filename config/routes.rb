4ca6137ca8cedbde2f96c38009ca7708e888c2cf
Michael Hauser-Raspe (home) <michael.hauser-raspe@cantab.net>
2013-10-01 11:37:04 +0100

    static files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 CRM::Application.routes.draw do
+  # static pages
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  root to: 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
